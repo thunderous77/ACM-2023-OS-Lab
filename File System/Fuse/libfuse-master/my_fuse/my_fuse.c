14df170bbc01417c479daab267b4536a4bd3228d
thunderous77 <zxc82921764@126.com>
2023-05-28 02:00:12 +0800

    finish the bonus of FUSE: permission check

--- File System/Fuse/libfuse-master/my_fuse/my_fuse.c
+++ File System/Fuse/libfuse-master/my_fuse/my_fuse.c
@@ -1,5 +1,5 @@
-// compile with "gcc -Wall rbtree.c my_fuse.c `pkg-config fuse3 --cflags --libs`
-// -o myfuse"
+// compile with "gcc -Wall my_fuse.c `pkg-config fuse3 --cflags --libs` -o
+// myfuse"
 #include <asm-generic/errno-base.h>
 #include <sys/stat.h>
 #define FUSE_USE_VERSION 31
@@ -12,36 +12,51 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "rbtree.h"
 #include <pthread.h>
 #include <stdlib.h>
 
 #define MAX_NODES 1000
 
-static struct options {
+typedef struct options options;
+typedef struct filenode filenode;
+typedef struct memfs_file memfs_file;
+
+struct options {
   char *filename;
   char *contents;
   int show_help;
-} options[MAX_NODES];
+} option[MAX_NODES];
 
 #define OPTION(t, p)                                                           \
-  { t, offsetof(struct options, p), 1 }
+  { t, offsetof(options, p), 1 }
 
 static const struct fuse_opt option_spec[] = {
     OPTION("--name=%s", filename), OPTION("--contents=%s", contents),
     OPTION("-h", show_help), OPTION("--help", show_help), FUSE_OPT_END};
 
-// File inodes build on rbtree
-struct rb_root root = RB_ROOT;
+filenode *head = NULL;
+
+struct filenode {
+  char *path;
+  filenode *next;
+  memfs_file *file;
+};
 
 struct memfs_file {
-  struct rb_node node;
   char *path;
-  struct options *option;
+  options *option;
   int dir_or_file; // 1: dir 2: file
 
   struct stat file_stat; // file attributes
 };
+
+// create a filenode
+filenode *create_filenode(char *path) {
+  filenode *new_node = malloc(sizeof(filenode));
+  new_node->path = strdup(path);
+  new_node->next = NULL;
+  return new_node;
+}
 
 // 返回一个字符串最后一个 '/' 后的字符串
 const char *getSubstringAfterLastSlash(const char *str) {
@@ -58,7 +73,10 @@ static inline const char *__is_parent(const char *parent, const char *path) {
   const char slash = '/';
 
   if (parent[1] == '\0' && parent[0] == '/' && path[0] == '/') {
-    return path;
+    if (path[1] != '\0')
+      return path;
+    else
+      return NULL;
   }
 
   while (*parent != '\0' && *path != '\0' && *parent == *path) {
@@ -67,65 +85,89 @@ static inline const char *__is_parent(const char *parent, const char *path) {
   return (*parent == '\0' && *path == slash) ? path : NULL;
 }
 
-// Reference: https://blog.csdn.net/stayneckwind2/article/details/82867062
-// insert inode into rbtree
-static int __insert(struct rb_root *root, struct memfs_file *pf) {
-  struct rb_node **new = &(root->rb_node), *parent = NULL;
-
-  while (*new) {
-    struct memfs_file *this = container_of(*new, struct memfs_file, node);
-    int result = strcmp(pf->path, this->path);
-    parent = *new;
-    if (result < 0)
-      new = &((*new)->rb_left);
-    else if (result > 0)
-      new = &((*new)->rb_right);
-    else
-      return -1;
+static int __insert(memfs_file *pf) {
+  filenode *new_node = create_filenode(pf->path);
+  if (head != NULL) {
+    new_node->file = pf;
+    new_node->next = head->next;
+    head->next = new_node;
+  } else {
+    new_node->file = pf;
+    head = new_node;
   }
-  rb_link_node(&pf->node, parent, new);
-  rb_insert_color(&pf->node, root);
   return 0;
 }
 
-// search inode in rbtree
-static struct memfs_file *__search(struct rb_root *root, const char *path) {
-  struct rb_node *node = root->rb_node;
-  struct memfs_file *data = NULL;
+static memfs_file *__search(const char *path) {
+  filenode *node = head;
+  memfs_file *mfile = NULL;
 
   while (node) {
-    data = container_of(node, struct memfs_file, node);
-    int result = strcmp(path, data->path);
+    mfile = node->file;
+    int result = strcmp(path, mfile->path);
 
-    if (result < 0)
-      node = node->rb_left;
-    else if (result > 0)
-      node = node->rb_right;
+    if (result != 0)
+      node = node->next;
     else
-      return data;
+      return mfile;
   }
   return NULL;
 }
 
-// free inode
-static void __free(struct memfs_file *pf) {
-  if (pf->path)
-    free(pf->path);
-  if (pf->option)
-    free(pf->option);
-  free(pf);
+static void __free(const char *path) {
+  filenode *node = head;
+  filenode *pre = NULL;
+  while (node) {
+    if (strcmp(path, node->path) == 0) {
+      if (pre == NULL) {
+        head = node->next;
+      } else {
+        pre->next = node->next;
+      }
+      free(node);
+      break;
+    }
+    pre = node;
+    node = node->next;
+  }
 }
 
-// delete inode from rbtree
-static int __delete(struct rb_root *root, const char *path) {
-  int res = 0;
-  struct memfs_file *pf = __search(root, path);
+static int __delete(const char *path) {
+  memfs_file *pf = __search(path);
   if (!pf) {
     return -1;
   }
-  rb_erase(&pf->node, root);
-  __free(pf);
-  return res;
+  __free(pf->path);
+  return 0;
+}
+
+// check the permission
+// permission: 0: read 1: write 2: execute
+static inline int __check_permission(const char *path, int permission) {
+  memfs_file *pf = __search(path);
+  if (!pf) {
+    return -ENOENT;
+  }
+  if (permission == 0) {
+    if (pf->file_stat.st_mode & S_IRUSR) {
+      return 0;
+    } else {
+      return -EACCES;
+    }
+  } else if (permission == 1) {
+    if (pf->file_stat.st_mode & S_IWUSR) {
+      return 0;
+    } else {
+      return -EACCES;
+    }
+  } else if (permission == 2) {
+    if (pf->file_stat.st_mode & S_IXUSR) {
+      return 0;
+    } else {
+      return -EACCES;
+    }
+  }
+  return 0;
 }
 
 static void *my_fuse_init(struct fuse_conn_info *conn,
@@ -133,14 +175,19 @@ static void *my_fuse_init(struct fuse_conn_info *conn,
   (void)conn;
   cfg->kernel_cache = 1;
   char *root_path = "/";
-  struct options *new_option = malloc(sizeof(struct options));
+  options *new_option = malloc(sizeof(options));
   new_option->filename = strdup(getSubstringAfterLastSlash(root_path));
   new_option->contents = strdup("");
-  struct memfs_file *new_node = malloc(sizeof(struct memfs_file));
+  memfs_file *new_node = malloc(sizeof(memfs_file));
   new_node->path = strdup(root_path);
   new_node->option = new_option;
   new_node->dir_or_file = 1;
-  __insert(&root, new_node);
+  new_node->file_stat.st_mode = S_IFDIR | 0755;
+  new_node->file_stat.st_nlink = 2;
+  new_node->file_stat.st_atime = time(NULL);
+  new_node->file_stat.st_mtime = time(NULL);
+  new_node->file_stat.st_ctime = time(NULL);
+  __insert(new_node);
   return NULL;
 }
 
@@ -151,33 +198,26 @@ static int my_fuse_getattr(const char *path, struct stat *stbuf,
 
   memset(stbuf, 0, sizeof(struct stat));
 
-  if (strcmp(path, "/") == 0) {
-    stbuf->st_mode = S_IFDIR | 0755;
-    stbuf->st_nlink = 2;
-  } else {
-    struct memfs_file *pf = __search(&root, path);
-    if (!pf) {
-      return -ENOENT;
-    }
-
-    // memcpy(stbuf, &pf->file_stat, sizeof(struct stat));
-    if (pf->dir_or_file == 2 && strcmp(path, pf->path) == 0) {
-      stbuf->st_mode = S_IFREG | 0444;
-      stbuf->st_nlink = 1;
-      stbuf->st_size = strlen(pf->option->contents);
-    } else if (pf->dir_or_file == 1) {
-      stbuf->st_mode = S_IFDIR | 0755;
-      stbuf->st_nlink = 2;
-    } else {
-      return -ENOENT;
-    }
+  memfs_file *pf = __search(path);
+  if (!pf) {
+    return -ENOENT;
   }
+
+  // memcpy(stbuf, &pf->file_stat, sizeof(struct stat));
+  stbuf->st_mode = pf->file_stat.st_mode;
+  stbuf->st_nlink = pf->file_stat.st_nlink;
+  stbuf->st_size = pf->file_stat.st_size;
 
   return 0;
 }
 
 // check file access permissions
-static int my_fuse_access(const char *path, int mask) { return 0; }
+static int my_fuse_access(const char *path, int mask) {
+  if (__check_permission(path, mask) != 0) {
+    return -EACCES;
+  }
+  return 0;
+}
 
 // read directory
 static int my_fuse_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
@@ -192,24 +232,23 @@ static int my_fuse_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
     filler(buf, "..", NULL, 0, 0);
   }
 
-  struct rb_node *node;
-  struct memfs_file *data;
+  memfs_file *data;
 
-  struct memfs_file *parent = __search(&root, path);
+  memfs_file *parent = __search(path);
   if (!parent) {
     return -ENOENT;
   }
 
-  for (node = rb_next(&parent->node); node; node = rb_next(node)) {
-    data = rb_entry(node, struct memfs_file, node);
+  filenode *fnode = head;
+  for (fnode = head; fnode; fnode = fnode->next) {
+    if (__check_permission(fnode->path, 0) != 0) {
+      continue;
+    }
+    data = fnode->file;
     const char *child = __is_parent(path, data->path);
 
-    if (!child) {
-      break;
-    }
-
     // only return children in first level, skip "/xxx/..."
-    if (strchr(child + 1, '/')) {
+    if (child == NULL || strchr(child + 1, '/')) {
       continue;
     }
 
@@ -228,9 +267,12 @@ static int my_fuse_open(const char *path, struct fuse_file_info *fi) {
 static int my_fuse_read(const char *path, char *buf, size_t size, off_t offset,
                         struct fuse_file_info *fi) {
   (void)fi;
-  struct memfs_file *pf = __search(&root, path);
+  memfs_file *pf = __search(path);
   if (!pf) {
     return -ENOENT;
+  }
+  if (__check_permission(path, 0) != 0) {
+    return -EACCES;
   }
 
   size_t len = strlen(pf->option->contents);
@@ -247,11 +289,13 @@ static int my_fuse_read(const char *path, char *buf, size_t size, off_t offset,
 // write data to an open file
 static int my_fuse_write(const char *path, const char *buf, size_t size,
                          off_t offset, struct fuse_file_info *fi) {
-  // printf("write %s\n", path);
   (void)fi;
-  struct memfs_file *pf = __search(&root, path);
+  memfs_file *pf = __search(path);
   if (!pf) {
     return -ENOENT;
+  }
+  if (__check_permission(path, 1) != 0) {
+    return -EACCES;
   }
 
   // write to the original file
@@ -263,6 +307,7 @@ static int my_fuse_write(const char *path, const char *buf, size_t size,
   strcpy(pf->option->contents, store);
   strcat(pf->option->contents, buf);
   pf->option->contents[len + size] = '\0';
+  pf->file_stat.st_size = len + size + 1;
 
   // write to another file in order to support chatting
   // example: echo "Hello" > bot2/bot1, write "Hello" to bot1/bot2
@@ -272,7 +317,7 @@ static int my_fuse_write(const char *path, const char *buf, size_t size,
     strcpy(new_path, tmp);
     strncat(new_path, path, tmp - path);
 
-    struct memfs_file *pf2 = __search(&root, new_path);
+    memfs_file *pf2 = __search(new_path);
 
     if (!pf2) {
       return -ENOENT;
@@ -285,7 +330,8 @@ static int my_fuse_write(const char *path, const char *buf, size_t size,
 
     strcpy(pf2->option->contents, store2);
     strcat(pf2->option->contents, buf);
-    pf2->option->contents[len2 + size] = '\0';  
+    pf2->option->contents[len2 + size] = '\0';
+    pf2->file_stat.st_size = len2 + size + 1;
   }
   return size;
 }
@@ -297,20 +343,25 @@ static int my_fuse_release(const char *path, struct fuse_file_info *fi) {
 
 // create a file node
 static int my_fuse_mknod(const char *path, mode_t mode, dev_t rdev) {
-  const struct memfs_file *pf = __search(&root, path);
+  memfs_file *pf = __search(path);
   if (pf) {
     return -EEXIST;
   }
 
   // create the original file
-  struct options *new_option = malloc(sizeof(struct options));
+  options *new_option = malloc(sizeof(options));
   new_option->filename = strdup(getSubstringAfterLastSlash(path));
   new_option->contents = strdup("");
-  struct memfs_file *new_node = malloc(sizeof(struct memfs_file));
+  memfs_file *new_node = malloc(sizeof(memfs_file));
   new_node->path = strdup(path);
   new_node->option = new_option;
   new_node->dir_or_file = 2;
-  __insert(&root, new_node);
+  new_node->file_stat.st_mode = S_IFREG | mode;
+  new_node->file_stat.st_nlink = 1;
+  new_node->file_stat.st_atime = time(NULL);
+  new_node->file_stat.st_mtime = time(NULL);
+  new_node->file_stat.st_ctime = time(NULL);
+  __insert(new_node);
 
   // create another file in order to support chatting
   char *tmp;
@@ -319,75 +370,91 @@ static int my_fuse_mknod(const char *path, mode_t mode, dev_t rdev) {
     strcpy(new_path, tmp);
     strncat(new_path, path, tmp - path);
 
-    struct memfs_file *new_pf = __search(&root, new_path);
+    memfs_file *new_pf = __search(new_path);
 
     if (new_pf) {
       return -EEXIST;
     }
 
-    struct options *new_option = malloc(sizeof(struct options));
+    options *new_option = malloc(sizeof(options));
     new_option->filename = strdup(getSubstringAfterLastSlash(new_path));
     new_option->contents = strdup("");
-    struct memfs_file *new_node = malloc(sizeof(struct memfs_file));
+    memfs_file *new_node = malloc(sizeof(memfs_file));
     new_node->path = strdup(new_path);
     new_node->option = new_option;
     new_node->dir_or_file = 2;
-    __insert(&root, new_node);
+    new_node->file_stat.st_mode = S_IFREG | mode;
+    new_node->file_stat.st_nlink = 1;
+    new_node->file_stat.st_atime = time(NULL);
+    new_node->file_stat.st_mtime = time(NULL);
+    new_node->file_stat.st_ctime = time(NULL);
+
+    __insert(new_node);
   }
   return 0;
 }
 
 // remove a file
 static int my_fuse_unlink(const char *path) {
-  const struct memfs_file *pf = __search(&root, path);
+  const memfs_file *pf = __search(path);
   if (!pf) {
     return -ENOENT;
   }
-  __delete(&root, path);
+  __delete(path);
   return 0;
 }
 
 // create a directory
 static int my_fuse_mkdir(const char *path, mode_t mode) {
-  const struct memfs_file *pf = __search(&root, path);
+  const memfs_file *pf = __search(path);
   if (pf) {
     return -EEXIST;
   }
 
-  struct options *new_option = malloc(sizeof(struct options));
+  options *new_option = malloc(sizeof(options));
   new_option->filename = strdup(getSubstringAfterLastSlash(path));
   new_option->contents = strdup("");
-  struct memfs_file *new_node = malloc(sizeof(struct memfs_file));
+  memfs_file *new_node = malloc(sizeof(memfs_file));
   new_node->path = strdup(path);
   new_node->option = new_option;
   new_node->dir_or_file = 1;
-  __insert(&root, new_node);
+  new_node->file_stat.st_mode = S_IFDIR | mode;
+  new_node->file_stat.st_nlink = 2;
+  new_node->file_stat.st_atime = time(NULL);
+  new_node->file_stat.st_mtime = time(NULL);
+  new_node->file_stat.st_ctime = time(NULL);
+  __insert(new_node);
   return 0;
 }
 
 // remove a directory
 static int my_fuse_rmdir(const char *path) {
   int res = 0;
-  if ((res = __delete(&root, path)) < 0) {
+  if ((res = __delete(path)) < 0) {
     return -ENOENT;
   }
   return res;
 }
 
-// change the access and modification times of a file with nanosecond
+// create a file or modify the time of a file
 static int my_fuse_utimes(const char *path, const struct timespec tv[2],
                           struct fuse_file_info *fi) {
   (void)fi;
-  struct memfs_file *pf = __search(&root, path);
+  memfs_file *pf = __search(path);
   if (!pf) {
-    struct options *new_option = malloc(sizeof(struct options));
+    options *new_option = malloc(sizeof(options));
     new_option->filename = strdup(getSubstringAfterLastSlash(path));
     new_option->contents = strdup("");
-    struct memfs_file *new_node = malloc(sizeof(struct memfs_file));
+    memfs_file *new_node = malloc(sizeof(memfs_file));
     new_node->path = strdup(path);
     new_node->option = new_option;
     new_node->dir_or_file = 2;
-    __insert(&root, new_node);
+    new_node->file_stat.st_mode = S_IFREG | 0644;
+    new_node->file_stat.st_nlink = 1;
+    new_node->file_stat.st_atime = time(NULL);
+    new_node->file_stat.st_mtime = time(NULL);
+    new_node->file_stat.st_ctime = time(NULL);
+    __insert(new_node);
   }
 
   // utimensat(0, path, tv, AT_SYMLINK_NOFOLLOW);
@@ -399,28 +466,41 @@ static int my_fuse_utimes(const char *path, const struct timespec tv[2],
   return 0;
 }
 
-static const struct fuse_operations my_fuse_oper = {
-    .init = my_fuse_init,
-    .getattr = my_fuse_getattr,
-    .access = my_fuse_access,
-    .readdir = my_fuse_readdir,
-    .open = my_fuse_open,
-    .read = my_fuse_read,
-    .write = my_fuse_write,
-    .release = my_fuse_release,
-    .mknod = my_fuse_mknod,
-    .unlink = my_fuse_unlink,
-    .mkdir = my_fuse_mkdir,
-    .rmdir = my_fuse_rmdir,
-    .utimens = my_fuse_utimes,
-};
+static int my_fuse_chmod(const char *path, mode_t mode,
+                         struct fuse_file_info *fi) {
+  (void)fi;
+  memfs_file *pf = __search(path);
+  if (pf) {
+    if (pf->dir_or_file == 1)
+      pf->file_stat.st_mode = S_IFDIR | mode;
+    else if (pf->dir_or_file == 2)
+      pf->file_stat.st_mode = S_IFREG | mode;
+  }
+
+  return 0;
+}
+
+static const struct fuse_operations my_fuse_oper = {.init = my_fuse_init,
+                                                    .getattr = my_fuse_getattr,
+                                                    .access = my_fuse_access,
+                                                    .readdir = my_fuse_readdir,
+                                                    .open = my_fuse_open,
+                                                    .read = my_fuse_read,
+                                                    .write = my_fuse_write,
+                                                    .release = my_fuse_release,
+                                                    .mknod = my_fuse_mknod,
+                                                    .unlink = my_fuse_unlink,
+                                                    .mkdir = my_fuse_mkdir,
+                                                    .rmdir = my_fuse_rmdir,
+                                                    .utimens = my_fuse_utimes,
+                                                    .chmod = my_fuse_chmod};
 
 int main(int argc, char *argv[]) {
   int ret;
   struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
   // options.filename = strdup("hello");
   // options.contents = strdup("Hello World!\n");
-  if (fuse_opt_parse(&args, &options, option_spec, NULL) == -1)
+  if (fuse_opt_parse(&args, &option, option_spec, NULL) == -1)
     return 1;
 
   ret = fuse_main(args.argc, args.argv, &my_fuse_oper, NULL);
